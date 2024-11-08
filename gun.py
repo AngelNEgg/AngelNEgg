6d63aa1d1a2c90b8fbad9e6d4ab55ac5093b01e2
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-11-08 12:51:14 -0500

    Update gun.py

--- gun.py
+++ gun.py
@@ -1,30 +1,15 @@
+# Russian Roulette
+
 import random
-import time
 
-rng1 = random.randint(1,5)
-rng2 = random.randint(1,5)
-bullets = 6
+shell1 = random.randint(0,1)
+shell2 = random.randint(0,1)
+shell3 = random.randint(0,1)
+shell4 = random.randint(0,1)
+shell5 = random.randint(0,1)
 
-def shoot_func(s,x,y):
-    s -= 1
-    print("pew")
-    time.sleep(0.25)
-    shoot_func(s,x,y)
-    
-    if x == y:
-        print("jammed...")
-        time.sleep(3)
-        shoot_func(s,x,y)
-        
-    elif x != y:
-        s -= 1
-        print("pew")
-        time.sleep(0.25)
-        shoot_func(s,x,y)
-        
-    elif s == 0:
-        print("reloading...")
-        time.sleep(2.5)
-        shoot_func(s,x,y)
-        
-shoot_func(bullets,rng1,rng2)
+item = "_"
+
+print("Choices:")
+
+choice = input("What will you do?")
