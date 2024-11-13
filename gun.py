cdd53302f8df007e40cdf2fa28740fc28ceaa667
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-11-13 12:13:14 -0500

    Update gun.py

--- gun.py
+++ gun.py
@@ -8,8 +8,12 @@ shell3 = random.randint(0,1)
 shell4 = random.randint(0,1)
 shell5 = random.randint(0,1)
 
-item = "_"
+item = random.randint(1,4)
+if item == 1:
+    item = "Phone"
+elif item == 2:
+    item = "Eyeglass"
 
-print("Choices:")
+print("Choices:\n\tShoot Self: Guessing 'live' will result in you loosing a health point.\n\tShoot Dealer: Guessing 'blank' will result in the end of your turn.\n\tUse Item: Using an item will remove it from your inventory, and can only be\n\treplenished at the start of a round.")
 
 choice = input("What will you do?")
