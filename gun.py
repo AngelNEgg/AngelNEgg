00a66836443f72aa65ad9cf77d22a192a659c8d2
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-12-18 12:13:47 -0500

    Update gun.py

--- gun.py
+++ gun.py
@@ -1,6 +1,8 @@
 # Russian Roulette
 
 import random
+import time
+import simplegui
 
 shell1 = random.randint(0,1)
 shell2 = random.randint(0,1)
@@ -19,7 +21,7 @@ elif item == 3:
 else:
     item = "Cigarette"
 
-print("Choices:\n\t-Shoot Self: Guessing 'live' will result in you loosing a health point.\n\t-Shoot Dealer: Guessing 'blank' will result in the end of your turn.\n\t-Use Item: Using an item will remove it from your inventory, and can only be\n\t replenished at the start of a round.")
+print("Tutorial:\n\t-Shoot Self: Guessing 'live' will result in you loosing a health point.\n\t-Shoot Dealer: Guessing 'blank' will result in the end of your turn.\n\t-Use Item: Using an item will remove it from your inventory, and can only be\n\t replenished at the start of a round.")
 
 health = 3
 choice = input("What will you do? ")
@@ -30,13 +32,17 @@ def turnFunc():
     shell3 = random.randint(0,1)
     shell4 = random.randint(0,1)
     shell5 = random.randint(0,1)
-    rounds = random.randint(2,5)
+    
     if choice == "Shoot Self" and shell1 == 1:
+        time.sleep(2)
         print("You Died? That's too bad...")
         chioce = input(("Would you like another try? "))
         if choice == "yes":
             turnFunc()
         else:
             print("I'll be seeing you, then.")
-    
+            pass
+    elif choice == "Shoot Self" and shell1 == 0:
+        time.sleep(2)
+        print("The shell was blank.")
     
