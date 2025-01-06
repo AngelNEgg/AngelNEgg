83b57e25909df161c108edc5f145af9c82b63d4a
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2025-01-06 12:34:02 -0500

    Update KeyPress.py

--- KeyPress.py
+++ KeyPress.py
@@ -4,21 +4,32 @@ import simplegui
 
 message = "Welcome!"
 color = "_"
+state = "still"
+
+# Handler for scene
+def draw(canvas):
+    canvas.draw_circle(())
 
 # Handler for keydown
 def keydown(key):
     global message
+    global state
     ####### Add KEY_MAP to translate to key codes
     if key == simplegui.KEY_MAP["up"]:
         message = "Up arrow"
+        state = "moved"
     elif key == simplegui.KEY_MAP["down"]:
         message = "Down arrow"
+        state = "moved"
     elif key == simplegui.KEY_MAP["left"]:
         message = "Left arrow"
+        state = "moved"
     elif key == simplegui.KEY_MAP["right"]:
         message = "Right arrow"
+        state = "moved"
         
-frame = simplegui.create_frame("Home", 300, 200)
+frame = simplegui.create_frame("Home", 600, 600)
+frame.set_draw_handler(draw)
 frame.set_keydown_handler(keydown)
 
 # Start the frame animation
