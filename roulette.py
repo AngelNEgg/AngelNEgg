29b384009e82e067273e23f197d3910e3b06ec6e
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2025-04-22 09:32:50 -0400

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -1,30 +1,45 @@
-# Russian Roulette
+# Angel Nazaire
+# 4/22/2025
+# Period 2
+'''Video Game Coding Assignment/Project: Create your game on scratch, 
+or another free coding platform of your choice.  
+Your game must include instructions/directions, and 
+a final objective. (Still a work in progress)'''
 
+# Russian Roulette-Like Game
+
+# Imports
 import random
 import time
 import simplegui
 
+# Variables
+health = 3
+oppHealth = 3
 shell1 = random.randint(0,1)
 shell2 = random.randint(0,1)
 shell3 = random.randint(0,1)
 shell4 = random.randint(0,1)
 shell5 = random.randint(0,1)
 rounds = random.randint(2,5)
+chamber = 0
+item = "_"
 
-item = random.randint(1,4)
-if item == 1:
-    item = "Phone"
-elif item == 2:
-    item = "Eyeglass"
-elif item == 3:
-    item = "Knife"
-else:
-    item = "Cigarette"
-
-print("Tutorial:\n\t-Shoot Self: Guessing 'live' will result in you loosing a health point.\n\t-Shoot Dealer: Guessing 'blank' will result in the end of your turn.\n\t-Use Item: Using an item will remove it from your inventory, and can only be\n\t replenished at the start of a round.")
-
-health = 3
-choice = input("What will you do? ")
+# Functions
+def itemFunc():
+    item = random.randint(1,4)
+    if item == 1:
+        item = "Phone"
+        return item
+    elif item == 2:
+        item = "Eyeglass"
+        return item
+    elif item == 3:
+        item = "Knife"
+        return item
+    else:
+        item = "Cigarette"
+        return item
 
 def turnFunc():
     shell1 = random.randint(0,1)
@@ -32,17 +47,30 @@ def turnFunc():
     shell3 = random.randint(0,1)
     shell4 = random.randint(0,1)
     shell5 = random.randint(0,1)
+    chamber = (shell1+shell2+shell3+shell4+shell5)
+    if chamber == 5:
+        turnFunc()
+        return
+    else:
+        print(f"{chamber} Live, {5-chamber} Blank.")
+        return
     
-    if choice == "Shoot Self" and shell1 == 1:
-        time.sleep(2)
-        print("You Died? That's too bad...")
-        chioce = input(("Would you like another try? "))
-        if choice == "yes":
-            turnFunc()
-        else:
-            print("I'll be seeing you, then.")
-            pass
-    elif choice == "Shoot Self" and shell1 == 0:
-        time.sleep(2)
-        print("The shell was blank.")
+# Gameplay
+print("Tutorial:\n\t-Choose Self: Guessing 'live' will result in you loosing a health point.\n\t-Choose Opponent: Guessing 'blank' will result in the end of your turn.\n\t-Use Item: Using an item will remove it from your inventory, and can only be\n\t replenished at the start of a round.")
+turnFunc()
+choice = input("What will you do? ")
+
+
+if choice == "Choose Self" and shell1 == 1:
+    time.sleep(2)
+    print("You Lost? That's too bad...")
+    chioce = input(("Would you like another try? "))
+    if choice == "yes":
+        turnFunc()
+    else:
+        print("I'll be seeing you, then.")
+        pass
+elif choice == "Choose Self" and shell1 == 0:
+    time.sleep(2)
+    print("The shell was blank.")
     
