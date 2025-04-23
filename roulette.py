49b140d87e93b25c91daa1f2096d544b0ce842c6
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2025-04-23 12:58:04 -0400

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -23,22 +23,27 @@ shell4 = random.randint(0,1)
 shell5 = random.randint(0,1)
 rounds = random.randint(2,5)
 chamber = 0
-item = "_"
+item = " "
+score = 0
 
 # Functions
 def itemFunc():
     item = random.randint(1,4)
     if item == 1:
         item = "Phone"
+        print("Item:",item)
         return item
     elif item == 2:
         item = "Eyeglass"
+        print("Item:",item)
         return item
     elif item == 3:
-        item = "Knife"
+        item = "Double Down"
+        print("Item:",item)
         return item
     else:
-        item = "Cigarette"
+        item = "Soda"
+        print("Item:",item)
         return item
 
 def turnFunc():
@@ -48,7 +53,7 @@ def turnFunc():
     shell4 = random.randint(0,1)
     shell5 = random.randint(0,1)
     chamber = (shell1+shell2+shell3+shell4+shell5)
-    if chamber == 5:
+    if chamber == 0 or chamber == 5:
         turnFunc()
         return
     else:
@@ -56,21 +61,59 @@ def turnFunc():
         return
     
 # Gameplay
-print("Tutorial:\n\t-Choose Self: Guessing 'live' will result in you loosing a health point.\n\t-Choose Opponent: Guessing 'blank' will result in the end of your turn.\n\t-Use Item: Using an item will remove it from your inventory, and can only be\n\t replenished at the start of a round.")
+print("Tutorial: Five shells are in play, and you can decide to...\n\t-'Choose Self': Guessing 'live' will result in you loosing a health point.\n\t-'Choose Opponent': Guessing 'blank' will result in the end of your turn.\n\t-'Use Item': Using an item will remove it from your inventory, and can only be\n\t replenished at the start of a round.")
 turnFunc()
-choice = input("What will you do? ")
+itemFunc()
 
-
-if choice == "Choose Self" and shell1 == 1:
-    time.sleep(2)
-    print("You Lost? That's too bad...")
-    chioce = input(("Would you like another try? "))
-    if choice == "yes":
-        turnFunc()
-    else:
-        print("I'll be seeing you, then.")
-        pass
-elif choice == "Choose Self" and shell1 == 0:
-    time.sleep(2)
-    print("The shell was blank.")
+# Gameplay Functions
+def shell1Func():
+    choice = input("What will you do? ")
+    if choice == "Choose Self" and shell1 == 1:
+        time.sleep(2)
+        print("You lost a health point.")
+        chioce = input(("Do you want to use an item? "))
+        if choice == "Yes" and item == 4:
+            item = 0
+            turnFunc()
+            shell2Func()
+        elif choice == "Yes" and item != 4:
+            print("You don't have healing items.")
+            pass
+        else:
+            print("Understood. Moving on, then.")
+            turnFunc()
+            shell2Func()
+            pass
+        
+    elif choice == "Choose Self" and shell1 == 0:
+        time.sleep(2)
+        print("The shell was blank.")
+        score += 1
+        shell2Func()
     
+def shell2Func():
+    choice = input("What will you do? ")
+    if choice == "Choose Self" and shell1 == 1:
+        time.sleep(2)
+        print("You lost a health point.")
+        chioce = input(("Do you want to use an item? "))
+        if choice == "Yes" and item == 4:
+            item = 0
+            turnFunc()
+            #shell3Func()
+        elif choice == "Yes" and item != 4:
+            print("You don't have healing items.")
+            pass
+        else:
+            print("Understood. Moving on, then.")
+            turnFunc()
+            #shell3Func()
+            pass
+        
+    elif choice == "Choose Self" and shell2 == 0:
+        time.sleep(2)
+        print("The shell was blank.")
+        score += 1
+        #shell3Func()
+        
+shell1Func()
